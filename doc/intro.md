4a4dca3b19268a8766bf207e011be8cab54daccf
Jack Shi <shisoftgenius@gmail.com>
2015-06-26 00:48:07 +0800

    first init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dist.async
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dist/async_test.clj
@@ -0,0 +1,7 @@
+(ns dist.async-test
+  (:require [clojure.test :refer :all]
+            [dist.async :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
